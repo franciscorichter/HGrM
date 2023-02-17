408a67e20c523b9d62ac61fb925a9e7cd10977a4
Ernst Wit <ernstwit72@gmail.com>
2023-02-17 10:41:28 +0100

    Update lsm.bd_fast.R

--- R/lsm.bd_fast.R
+++ R/lsm.bd_fast.R
@@ -1,18 +1,23 @@
-lsm.bd_fast<-function(data,Z=NULL,initial.graphs=NULL, D=2, initial.cloc=NULL, initial.alpha=NULL, initial.beta=NULL, bd.iter=100,iter=1000,burnin=NULL, gcgm.dwpar=NULL)
+lsm.bd_fast<-function(data,X=NULL,iter=1000,burnin=0,initial.graphs=NULL, D=2, initial.loc=NULL, initial.alpha=NULL, initial.theta=NULL, bd.iter=20,bd.jump=10, method=c("ggm","gcgm"), gcgm.dwpar=NULL)
 {
   p<-ncol(data[[1]]) #number of nodes
   n.edge<-p*(p-1)/2 #number of edges
   B<-length(data) #number of conditions
-  pi.edgpost<-matrix(0,n.edge,B)
-  khat.edgpost=NULL
   
   sample.graphs<-array(dim=c(n.edge,B,iter))
+  sample.K<-array(dim=c(n.edge,B,iter))
+  sample.pi<-array(dim=c(n.edge,B,iter))
+  
+  # lower triangle of pxp matrix
+  m<-matrix(1:p,ncol=p,nrow = p)
+  lt<-lower.tri(m)
+
   if(is.null(initial.graphs))
   {
     for(i in 1:B)
     {
       g<-huge.select(huge(as.matrix(data[[i]]),method="glasso"),criterion="stars")$refit
-      sample.graphs[,i,1]<-g[lower.tri(g)]
+      sample.graphs[,i,1]<-g[lt]
     }
   }
   else
@@ -21,46 +26,71 @@ lsm.bd_fast<-function(data,Z=NULL,initial.graphs=NULL, D=2, initial.cloc=NULL, i
   sample.cloc<-array(dim = c(B,D,iter))
   sample.alpha<-matrix(0,B,iter)
   
-  if(is.null(initial.cloc))
+  if(is.null(initial.loc))
     sample.cloc[,,1]<-matrix(rnorm(B*D),ncol=D)
   else
-    sample.cloc[,,1]<-initial.cloc
+    sample.cloc[,,1]<-initial.loc
   
-  if(!is.null(Z))
+  Z<-X
+  if(!is.null(X))
   {
-    Z<-as.matrix(Z)
+    Z<-as.matrix(X)
     sample.beta<-matrix(0,ncol(Z),iter)
-    if(is.null(initial.beta))
+    if(is.null(initial.theta))
     {
       y<-as.vector(sample.graphs[,,1])
       X<-apply(Z,2,rep,B)
       sample.beta[,1]<-coef(glm(y~X, family=binomial(link = "probit")))[-1]
     }
-    if(!is.null(initial.beta))
-      sample.beta[,1]<-initial.beta
+    if(!is.null(initial.theta))
+      sample.beta[,1]<-initial.theta
   }
-  log.graph.prob<-NULL
   
   if(is.null(burnin))
     burnin<-floor(0.75*iter)
   
-  m<-matrix(1:p,ncol=p,nrow = p)
-  e1<-t(m)[lower.tri(m)]
-  e2<-m[lower.tri(m)]
+  # edge indicators
+  e1<-t(m)[lt]
+  e2<-m[lt]
   
+    
   
   # Initialize K (precision matrix)
   K=vector(mode="list", length=B)
   for (i in 1:B){
     K[[i]]<-diag(p)
+    sample.K[,i,1]<-K[[i]][lt]
   }
   
+  
+  if (method=="gcgm"){
+    discrete.data<-data
+    #calculate truncated points
+    tpoints<-vector("list",B)
+    for(i in 1:B)
+    {
+      tpoints[[i]]<-vector("list",2)
+      beta.dw<-gcgm.dwpar[[i]]$beta
+      q<-gcgm.dwpar[[i]]$q
+      pii<-matrix(rep(gcgm.dwpar[[i]]$pii,each=nrow(q)),nrow(q),ncol(q))
+      pdw_lb = BDgraph::pdweibull( data[[i]] - 1, q = q, beta = beta.dw)
+      pdw_ub = BDgraph::pdweibull( data [[i]], q = q, beta = beta.dw)
+      tpoints[[i]][[1]]<-stats::qnorm( ( 1 - pii)*( data[[i]] != 0 ) + pii*pdw_lb)
+      tpoints[[i]][[2]] <- stats::qnorm( (1 - pii) + pii*pdw_ub)
+    }
+  }
   
   pb <- txtProgressBar(min = 0, max = (iter-1), style = 3)
   for (k in 1: (iter-1))
   {
     setTxtProgressBar(pb = pb, value = k,title = "Performing MCMC iterations") 
 
+    # update data if the Gaussian Copula GM (gcgm) is selected
+    if (method=="gcgm"){
+      data<-sample.data(data,discrete.data, K, tpoints)
+    }
+    
+    
     # update latent node and condition locations
     G<-sample.graphs[,,k]
     if(is.null(Z))
@@ -71,7 +101,6 @@ lsm.bd_fast<-function(data,Z=NULL,initial.graphs=NULL, D=2, initial.cloc=NULL, i
     cloc<-G.loc$cloc[,,1]
     alpha<-G.loc$alpha
     beta<- G.loc$beta
-    log.graph.prob<-c(log.graph.prob,G.loc$log.graph.prob)
     
     dist.cond<-matrix(ncol=B,nrow=n.edge)
     for (b in 1:B){
@@ -90,44 +119,40 @@ lsm.bd_fast<-function(data,Z=NULL,initial.graphs=NULL, D=2, initial.cloc=NULL, i
         }
       }
     }
-    K<-vector("list",B)
+
     for (j in 1:B)
     {
       pi.post<-Pi[,j]
       g.prior<-matrix(0,nrow=p,ncol=p)
-      g.prior[lower.tri(g.prior)] <- pi.post
+      g.prior[lt] <- pi.post
       g.prior<-g.prior+t(g.prior)
       
       g.start<-matrix(0,nrow=p,ncol=p)
-      g.start[lower.tri(g.start)] <- sample.graphs[,j,k]
+      g.start[lt] <- sample.graphs[,j,k]
       g.start<-g.start+t(g.start)
-      if(k < (iter-1))
-        # update K
-        res.bd<-BDgraph::bdgraph(data[[j]], iter = bd.iter, g.start=g.start,  g.prior=g.prior, save=FALSE, burnin=0,verbose = FALSE)
-      else
-      {
-        res.bd<-BDgraph::bdgraph(data[[j]], iter = bd.iter*100, g.start=g.start,  g.prior=g.prior, save=TRUE, burnin=0, verbose = FALSE)
-        pp<-plinks(res.bd)
-        pi.edgpost[,j]<-t(pp)[lower.tri(pp)]
-        khat.edgpost[[j]]<-res.bd$K_hat[lower.tri(res.bd$K_hat)]
-        
-      }
+      
+      # update K
+      res.bd<-BDgraph::bdgraph(data[[j]], iter = bd.iter, jump=bd.jump, g.start=g.start,  g.prior=g.prior, save=FALSE, burnin=0,verbose = FALSE)
+     
       g<-res.bd$last_graph
       K[[j]]<-res.bd$last_K
-      sample.graphs[,j,k+1]<-g[lower.tri(g)]
-      
-      
+      sample.graphs[,j,k+1]<-g[lt]
+      sample.K[,j,k+1] <- K[[j]][lt]
+      sample.pi[,j,k+1] <- t(res.bd$p_links)[lt]
     }
+    
     sample.cloc[,,k+1]<-cloc
     sample.alpha[,k+1]<-alpha
     if(!is.null(Z))
       sample.beta[,k+1]<-beta
-    
   }
   
   sample.cloc<-sample.cloc[,,-(1:burnin)]
   sample.alpha<-sample.alpha[,-(1:burnin)]
   sample.graphs<-sample.graphs[,,-(1:burnin)]
+  sample.K<-sample.K[,,-(1:burnin)]
+  sample.pi<-sample.pi[,,-(1:burnin)]
+  
   if(!is.null(Z))
     sample.beta<-sample.beta[,-(1:burnin),drop=FALSE]
   
@@ -159,9 +184,9 @@ lsm.bd_fast<-function(data,Z=NULL,initial.graphs=NULL, D=2, initial.cloc=NULL, i
     }
   }
   if(is.null(Z))
-    return(list(sample.alpha=sample.alpha,sample.cloc=sample.cloc,khat.edgpost=khat.edgpost,sample.graphs=sample.graphs,pi.edgpost=pi.edgpost,pi.probit=pi.probit))
+    return(list(sample.alpha=sample.alpha,sample.loc=sample.cloc,sample.K=sample.K,sample.graphs=sample.graphs,sample.pi=sample.pi,pi.probit=pi.probit))
   else
-    return(list(sample.alpha=sample.alpha,sample.beta=sample.beta,khat.edgpost=khat.edgpost,sample.cloc=sample.cloc,sample.graphs=sample.graphs,pi.edgpost=pi.edgpost,pi.probit=pi.probit))
+    return(list(sample.alpha=sample.alpha,sample.beta=sample.beta,sample.K=sample.K,sample.loc=sample.cloc,sample.graphs=sample.graphs,sample.pi=sample.pi,pi.probit=pi.probit))
 }
 
 
